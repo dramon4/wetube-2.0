d191c632aa17b6a143040fe29542ad012fc0aa30
KingMeTo92 <34463714+dramon4@users.noreply.github.com>
2019-07-05 14:57:18 +0900

    #2.18 Search Controller

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,8 +1,15 @@
 export const home = (req, res) => res.render("home", { pageTitle: "Home" });
-export const search = (req, res) =>
-  res.render("search", { pageTitle: "Search" });
+export const search = (req, res) => {
+  // const searchingBy = req.query.term;
+  const {
+    query: { term: searchingBy }
+  } = req;
+  res.render("search", { pageTitle: "Search", searchingBy });
+};
 export const upload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
+export const videos = (req, res) =>
+  res.render("videos", { pageTitle: "Videos" });
 export const videoDetail = (req, res) =>
   res.render("videoDetail", { pageTitle: "Video Detail" });
 export const editVideo = (req, res) =>
